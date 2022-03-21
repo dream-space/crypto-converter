7119dd28659bce159e55d390db2a1880b4f0eb8f
Muslim Sidiq <muslimsidiq2@gmail.com>
2022-03-22 06:26:29 +0700

    implement gecko coin

--- index.php
+++ index.php
@@ -1,63 +1,22 @@
 <?php
 require_once("rest.php");
+function requestAPI($amount, $from, $to){
+    $url = "https://api.coingecko.com/api/v3/simple/price?ids=$from&vs_currencies=$to&include_last_updated_at=true";
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-function getDOMXPath($amount, $from, $to){
-    $from = strtoupper($from);
-    $to = strtoupper($to);
-    try {
-        $data = fileContents(
-            "https://www.xe.com/en/currencyconverter/convert/?Amount=$amount&From=$from&To=$to".'&t='.mt_rand());
-    } catch (Exception $e) {
-        return -2;
-    }
-
-    //var_dump($data);
-    $doc = new DOMDocument;
-    libxml_use_internal_errors(true);
-    $doc->loadHTML($data);
-    libxml_use_internal_errors(false);
-    return $doc;
+    $output = curl_exec($curl);
+    curl_close($curl);
+    return $output;
 }
 
-function getRateValue($doc) {
-	$xpath = new DOMXPath($doc);
-	$result = $xpath->query('//p[starts-with(@class,"result__BigRate")]');
-	if($result->length > 0) {
-	  $node = $result->item(0);
-	  $value = $node->nodeValue;
-	  return floatValue(explode(" ", $value)[0]);
-	} else {
-	  return -1;
-	}
+function getDateUTC($date_time){
+    $dt = new DateTime();
+    $dt->setTimestamp($date_time);
+    $dt->setTimezone(new DateTimeZone('UTC'));
+    return $dt->format('M d, Y, H:i e');
 }
-
-function getDateValue($doc){
-    $result_arr = array(
-        'date' => '',
-        'date_time' => 0
-    );
-    $xpath = new DOMXPath($doc);
-    $result = $xpath->query('//div[starts-with(@class,"result__LiveSubText")]');
-    if($result->length > 0) {
-        $node = $result->item(0);
-        $value = $node->nodeValue;
-        $value = explode("Last updated ", $value)[1];
-        $result_arr['date'] = $value;
-        $result_arr['date_time'] = strtotime($value);
-    }
-    return $result_arr;
-}
-
-// Returns the contents of a file
-function fileContents($path) {
-    $str = @file_get_contents($path);
-    if ($str === FALSE) {
-        throw new Exception("Cannot access '$path' to read contents.");
-    } else {
-        return $str;
-    }
-}
-
 
 function floatValue($val){
     $val = str_replace(",",".",$val);
@@ -92,19 +51,21 @@ $result = array(
 );
 
 if(!$multi){
-    $doc = getDOMXPath($amount, $from, $to);
-
-    $rate = getRateValue($doc);
-    $date = getDateValue($doc);
-    $result['date'] = $date['date'];
-    $result['date_time'] = $date['date_time'];
-    $result['rate'] = $rate;
+    $data = json_decode(requestAPI($amount, $from, $to), true);
+    $last_updated_at = $data[$from]['last_updated_at'];
+    $result['date'] = getDateUTC($last_updated_at);
+    $result['date_time'] = $last_updated_at;
+    $result['rate'] = $data[$from][$to];
 
 } else {
     $multi = array();
     foreach ($to_arr as $t) {
-        $doc = getDOMXPath($amount, $from, $t);
-        $rate = getRateValue($doc);
+        $data = json_decode(requestAPI($amount, $from, $t), true);
+        $last_updated_at = $data[$from]['last_updated_at'];
+        $rate = $data[$from][$t];
+        $result['date'] = getDateUTC($last_updated_at);
+        $result['date_time'] = $last_updated_at;
+
         $multi[] = array('to' => $t, 'rate' => $rate);
     }
     $result['multi'] = $multi;
